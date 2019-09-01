0e0cbd534b2004a1eaacbf2e52468ce927cf713b
Mike Cooper <mikescooper@icloud.com>
2019-09-01 05:46:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,12 @@ class Song
   
   attr_accessor :name, :artist, :genre 
   
+  def initialize(name, artist, genre)
+    @name = name 
+    @artist = artist 
+    @genre = genre
+  end 
+  
   @@count = 0 
   
 end
